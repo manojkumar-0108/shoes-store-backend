4ed821acc266d914bb8fa7ac1bb7ba30ad35a63b
Manoj Kumar <manoj8894421801@gmail.com>
2024-05-15 22:47:54 +0530

    Feat: added users routes and validations

---
+++ src/middlewares/user.middleware.js
@@ -0,0 +1,83 @@
+const { StatusCodes } = require('http-status-codes');
+const { AppError } = require("../errors");
+const { UserService } = require('../services');
+
+
+
+function validateRegisterRequest(req, res, next) {
+
+    if (!req.body.name || !req.body.email || !req.body.password) {
+
+        let details = new Array();
+
+        if (!req.body.name) {
+            details.push("name is not found in incomming request in correct form")
+        }
+
+        if (!req.body.email) {
+            details.push("email is not found in incomming request in correct form")
+        }
+
+        if (!req.body.password) {
+            details.push("password is not found in incomming request in correct form")
+        }
+
+        throw new AppError(StatusCodes.BAD_REQUEST, "Please enter valid details", details);
+    }
+
+    next();
+}
+
+function validateLoginRequest(req, res, next) {
+
+    if (!req.body.email || !req.body.password) {
+
+        let details = new Array();
+
+        if (!req.body.email) {
+            details.push("email is not found in incomming request in correct form")
+        }
+
+        if (!req.body.password) {
+            details.push("password is not found in incomming request in correct form")
+        }
+
+        throw new AppError(StatusCodes.BAD_REQUEST, "Please enter valid details", details);
+    }
+
+    next();
+}
+
+async function checkAuth(req, res, next) {
+    try {
+        const userService = new UserService();
+        const response = await userService.isAuthenticated(req.headers['x-access-token']);
+        if (response) {
+            req.user = response;
+            next();
+        }
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+async function isAdmin(req, res, next) {
+    try {
+        const userService = new UserService();
+        const response = await userService.isAdmin(req.user.id);
+        if (!response) {
+            throw new AppError(StatusCodes.UNAUTHORIZED, 'Cannot add role.', ['User not authorized for this action'])
+        }
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports = {
+    validateRegisterRequest,
+    validateLoginRequest,
+    checkAuth,
+    isAdmin
+};
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-
 const { pingCheck } = require('../../controllers');
 
-
+const userRouter = require('./user.routes');
 const v1Router = express.Router();
 
 
 v1Router.get('/ping', pingCheck('API version V1 is live...'));
 
+v1Router.use('/users', userRouter);
 
 module.exports = v1Router;
