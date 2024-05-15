aa022fbfcfa0a1b0eae253cb78b5a9311618d04d
Manoj Kumar <manoj8894421801@gmail.com>
2024-05-15 15:27:53 +0530

    FirstCommit: basic project setup

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * packages import statements
+ */
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+
+
+/**
+ * custom functions import statements
+ */
+const { serverConfig } = require('./config');
+
+
+/**
+ * Main 
+ */
+const app = express();
+
+//body parsing middlewares
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.text());
+app.use(express.raw());
+
+
+// Rate Limiter
+const limiter = rateLimit({
+    windowMs: 5 * 60 * 1000, // 5 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).  
+})
+app.use(limiter);
+
+
+
+app.listen(serverConfig.PORT, () => {
+    console.log(`Server started at port ${serverConfig.PORT}`);
+})
